1288860bf280a415c935ff5378bb98a20649b4ff
sachityadav-2007 <sachitslayers@gmail.com>
2026-03-01 10:23:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({ origin: "https://ai-contribution-detector.onrender.com" }));
+app.use(cors({ origin: "https://ai-contribution-detector.vercel.app/" }));
 app.use(express.json());
 
 app.post("/analyze", (req, res) => {
